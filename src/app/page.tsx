ddcb54ce80efcdffa314082f8632cf06014557ec
andrewarosario <andrew.arosario@gmail.com>
2023-07-11 16:51:22 -0300

    chore: add contentlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { allPosts } from 'contentlayer/generated';
+
 export default function Home() {
+  const posts = allPosts.map((post) => post);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello world</h1>
+      <div dangerouslySetInnerHTML={{ __html: posts[0]?.title }}></div>
     </main>
   );
 }
