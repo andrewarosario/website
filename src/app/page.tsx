91ca12a6df991705bc75280c0c85c3a80ecd1a9b
Andrew Rosario <andrew.arosario@gmail.com>
2023-07-12 09:12:14 -0300

    feat: add mdx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image';
+import { Mdx } from '@/components/Mdx';
 import { allPosts } from 'contentlayer/generated';
 
 export default function Home() {
   const posts = allPosts.map((post) => post);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world</h1>
-      <div dangerouslySetInnerHTML={{ __html: posts[0]?.title }}></div>
+      <Image src={posts[0].image} width={700} height={700} alt="" />
+      <Mdx code={posts[0].body.code}></Mdx>
     </main>
   );
 }
