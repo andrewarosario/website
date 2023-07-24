86d69f55a99db4c3567b955c28cdded2a4524d7c
andrewarosario <andrew.arosario@gmail.com>
2023-07-24 10:13:11 -0300

    refactor: add PostService

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import { allPosts } from 'contentlayer/generated';
-
 import { siteConfig } from '@/config';
+import { PostService } from '@/services';
 
 import { Grid } from '@/components/Grid';
 import { PostCard } from '@/components/PostCard';
 import { Profile } from '@/components/Profile';
 
 export default function Home() {
-  const posts = allPosts.map((post) => post);
+  const { posts } = PostService.getAll();
 
   return (
     <main>
