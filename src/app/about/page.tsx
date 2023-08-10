832cd2cef2f37d77638d8570c574808908d23c0e
andrewarosario <andrew.arosario@gmail.com>
2023-08-10 10:22:01 -0300

    feat: add about page metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,35 @@
+import { Metadata } from 'next';
 import Image from 'next/image';
+
+import { siteConfig } from '@/config';
 
 import { Link } from '@/components/Link';
 
 import * as S from './styles';
+
+export const metadata: Metadata = {
+  title: 'Sobre mim',
+  description: 'Sobre mim',
+  metadataBase: new URL(siteConfig.url),
+  openGraph: {
+    type: 'website',
+    title: 'Sobre mim',
+    url: '/about',
+    description: 'Sobre mim',
+    siteName: 'Sobre mim',
+    images: [
+      {
+        url: `${siteConfig.url}/assets/images/image-post.jpeg`
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Sobre mim',
+    description: 'Sobre mim',
+    images: [`${siteConfig.url}/assets/images/image-post.jpeg`]
+  }
+};
 
 export default function AboutPage() {
   return (
