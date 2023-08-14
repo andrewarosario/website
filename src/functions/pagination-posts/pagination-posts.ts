84acf594387dd000d9362d871a3a06836b347ad5
andrewarosario <andrew.arosario@gmail.com>
2023-08-14 11:45:40 -0300

    refactor: remove unused functions

--- src/functions/pagination-posts/pagination-posts.ts
+++
@@ -1,8 +0,0 @@
-import { BlogPost } from '@/types';
-
-export const paginationPosts = (posts: BlogPost[], limit = 10, page = 1) => {
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  return posts.slice(startIndex, endIndex);
-};
