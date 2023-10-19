0911fad6e84e1d7a1a675179f1eea0f61dbae56a
andrewarosario <andrew.arosario@gmail.com>
2023-10-19 18:18:10 -0300

    fix: change next response

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,19 +1,19 @@
 import { revalidateTag } from 'next/cache';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get('secret');
   const tag = request.nextUrl.searchParams.get('tag');
 
   if (secret !== process.env.MEDIUM_API_REVALIDATION_TOKEN) {
-    return Response.json({ message: 'Invalid secret' }, { status: 401 });
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
   }
 
   if (!tag) {
-    return Response.json({ message: 'Missing tag param' }, { status: 400 });
+    return NextResponse.json({ message: 'Missing tag param' }, { status: 400 });
   }
 
   revalidateTag(tag);
 
-  return Response.json({ revalidated: true, now: Date.now() });
+  return NextResponse.json({ revalidated: true, now: Date.now() });
 }
