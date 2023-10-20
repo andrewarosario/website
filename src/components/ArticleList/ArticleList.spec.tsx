b66f13b19afc5ff07f04f3dd118c64fc7b306a54
andrewarosario <andrew.arosario@gmail.com>
2023-10-20 16:22:42 -0300

    test: add article list component tests

---
+++ src/components/ArticleList/ArticleList.spec.tsx
@@ -0,0 +1,25 @@
+import { site } from '@/config';
+import { mockArticles } from '@/mocks';
+import { render, screen } from '@testing-library/react';
+
+import { ArticleList } from './ArticleList';
+
+describe('ArticleList', () => {
+  it('should render the article list header', () => {
+    render(<ArticleList articles={mockArticles} />);
+    const heading = screen.getByText('Últimos posts');
+    expect(heading).toBeVisible();
+  });
+
+  it('should render the "Ver mais" link', () => {
+    render(<ArticleList articles={mockArticles} />);
+    const link = screen.getByText('Ver mais');
+    expect(link).toBeVisible();
+    expect(link).toHaveAttribute('href', site.links.blog);
+  });
+
+  it('should render one article card for each article in the list', () => {
+    render(<ArticleList articles={mockArticles} />);
+    expect(screen.getAllByRole('article')).toHaveLength(mockArticles.length);
+  });
+});
