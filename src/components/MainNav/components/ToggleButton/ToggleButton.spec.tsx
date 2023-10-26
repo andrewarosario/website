20805e50ec7f68a5a451bd481ef66e75108b10d8
andrewarosario <andrew.arosario@gmail.com>
2023-10-26 11:21:23 -0300

    feat: improve toggle button accessibility

--- src/components/MainNav/components/ToggleButton/ToggleButton.spec.tsx
+++ src/components/MainNav/components/ToggleButton/ToggleButton.spec.tsx
@@ -16,12 +16,14 @@ describe('ToggleButton', () => {
     };
     render(<ToggleButton {...props} />);
     const closeIcon = screen.getByTestId('close-icon');
+    const closeButton = screen.getByRole('button', { name: /fechar menu/i });
 
     act(() => {
       fireEvent.click(closeIcon);
     });
 
     expect(closeIcon).toBeVisible();
+    expect(closeButton).toBeVisible();
     expect(handleToggleMenu).toHaveBeenCalledTimes(1);
   });
 
@@ -32,12 +34,14 @@ describe('ToggleButton', () => {
     };
     render(<ToggleButton {...props} />);
     const openIcon = screen.getByTestId('open-icon');
+    const openButton = screen.getByRole('button', { name: /abrir menu/i });
 
     act(() => {
       fireEvent.click(openIcon);
     });
 
     expect(openIcon).toBeVisible();
+    expect(openButton).toBeVisible();
     expect(handleToggleMenu).toHaveBeenCalledTimes(1);
   });
 });
